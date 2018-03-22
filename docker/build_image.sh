aa429005c6670758fcf1d895f699b03250e8035b
Sastry Isukapalli <sastry@research.att.com>
2018-03-22 02:03:23 -0400

    Add docker components for CSIT
    Added license to Dockerfile
    Added 8699 internal port and 8698 external port
    
    Issue-ID: OPTFRA-22
    Change-Id: I232acc6cd417efda164ae825362dcc6d0be28a12
    Signed-off-by: Sastry Isukapalli <sastry@research.att.com>

---
+++ docker/build_image.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# The script starts in the root folder of the repo, which has the following outline
+# We fetch the version information from version.properties, build docker files and
+# do a docker push. Since the job will be run under Jenkins, it will have the Nexus
+# credentials
+#
+#  ├── docker
+#  │   ├── Dockerfile
+#  │   └── build_image.sh   <--- THIS SCRIPT is here
+#  ├── docs
+#  ├── osdf
+#  ├── pom.xml
+#  ├── test
+#  └── version.properties   <--- Version information here
+
+set -e
+
+# Folder settings
+DOCKER_REPOSITORY=nexus3.onap.org:10003
+ORG=onap
+PROJECT=optf-osdf
+IMAGE_NAME=$DOCKER_REPOSITORY/$ORG/$PROJECT
+
+# Version properties
+source version.properties
+VERSION=$release_version
+STAGING=${release_version}-SNAPSHOT-$(date -u +date -u '+%Y%m%dT%H%M%SZ')
+
+function log_ts() {  # Log message with timestamp
+    echo [DEBUG LOG at $(date -u +%Y%m%d:%H%M%S)] "$@"
+}
+
+function build_image() {
+     log_ts Building Image in folder: $PWD
+     docker build -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest .
+     log_ts ... Built
+}
+
+function push_image(){
+     log_ts Pushing images: ${IMAGE_NAME}:\{$VERSION,$STAGING,latest\}
+     docker push ${IMAGE_NAME}:${VERSION}
+     docker push ${IMAGE_NAME}:${STAGING}
+     docker push ${IMAGE_NAME}:latest
+     log_ts ... Pushed images
+}
+
+(
+  cd $(dirname $0)
+  build_image
+  push_image
+)
