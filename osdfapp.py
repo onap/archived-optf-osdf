dce2ae6338bf8edb579dc55adcdd6d32b832b55b
Chayal, Avteet (ac229e) <ac229e@att.com>
2018-09-19 00:35:17 +0000

    CVS changes for osdf placment api
    Implemented ONAP Common Versioning Strategy
    
    Issue-ID: OPTFRA-285
    Change-Id: I31df699afddbeb8962b2ca0fa501eff45f70ed5d
    Signed-off-by: Chayal, Avteet (ac229e) <ac229e@att.com>

--- osdfapp.py
+++ osdfapp.py
@@ -47,6 +47,7 @@ from osdf.logging.osdf_logging import MH, audit_log, error_log, debug_log
 from osdf.models.api.placementRequest import PlacementAPI
 from osdf.operation.responses import osdf_response_for_request_accept as req_accept
 from osdf.optimizers.routeopt.simple_route_opt import RouteOpt
+from osdf.utils import api_data_utils
 
 ERROR_TEMPLATE = osdf.ERROR_TEMPLATE
 
@@ -105,6 +106,16 @@ def do_osdf_health_check():
 @app.route("/api/oof/v1/placement", methods=["POST"])
 @auth_basic.login_required
 def do_placement_opt():
+    return placement_rest_api()
+
+
+@app.route("/api/oof/placement/v1", methods=["POST"])
+@auth_basic.login_required
+def do_placement_opt_common_versioning():
+    return placement_rest_api()
+
+
+def placement_rest_api():
     """Perform placement optimization after validating the request and fetching policies
     Make a call to the call-back URL with the output of the placement request.
     Note: Call to Conductor for placement optimization may have redirects, so account for them
@@ -113,6 +124,7 @@ def do_placement_opt():
     req_id = request_json['requestInfo']['requestId']
     g.request_id = req_id
     audit_log.info(MH.received_request(request.url, request.remote_addr, json.dumps(request_json)))
+    api_version_info = api_data_utils.retrieve_version_info(request, req_id)
     PlacementAPI(request_json).validate()
     policies = get_policies(request_json, "placement")
     audit_log.info(MH.new_worker_thread(req_id, "[for placement]"))
@@ -121,7 +133,7 @@ def do_placement_opt():
     audit_log.info(MH.accepted_valid_request(req_id, request))
     return req_accept(request_id=req_id,
                       transaction_id=request_json['requestInfo']['transactionId'],
-                      request_status="accepted", status_message="")
+                      version_info=api_version_info, request_status="accepted", status_message="")
 
 
 @app.route("/api/oof/v1/route", methods=["POST"])
