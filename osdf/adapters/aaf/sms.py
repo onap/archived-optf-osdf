bad41e08136e43c2b95e0235a6e34ee8623ac219
Chayal, Avteet (ac229e) <ac229e@att.com>
2020-03-19 03:13:27 -0400

    OSDF migration to new Policy API
    Map policy type with constraints type for HAS payload
    Added new attribute policies for Network Sliciing reference
    Fixed the logic to proccess Attribute policy translation
    
    Issue-ID: OPTFRA-721
    Change-Id: I6b01815963b48b3df97c1a3109cc39b2a8bc2c3c
    Signed-off-by: Chayal, Avteet (ac229e) <ac229e@att.com>

--- osdf/adapters/aaf/sms.py
+++ osdf/adapters/aaf/sms.py
@@ -73,35 +73,17 @@ def retrieve_secrets():
 
 def load_secrets():
     config = osdf_config.deployment
-    secret_dict = retrieve_secrets()
-    config['soUsername'] = secret_dict['so']['UserName']
-    config['soPassword'] = decrypt_pass(secret_dict['so']['Password'])
-    config['conductorUsername'] = secret_dict['conductor']['UserName']
-    config['conductorPassword'] = decrypt_pass(secret_dict['conductor']['Password'])
-    config['policyPlatformUsername'] = secret_dict['policyPlatform']['UserName']
-    config['policyPlatformPassword'] = decrypt_pass(secret_dict['policyPlatform']['Password'])
-    config['policyClientUsername'] = secret_dict['policyPlatform']['UserName']
-    config['policyClientPassword'] = decrypt_pass(secret_dict['policyPlatform']['Password'])
-    config['messageReaderAafUserId'] = secret_dict['dmaap']['UserName']
-    config['messageReaderAafPassword'] = decrypt_pass(secret_dict['dmaap']['Password'])
-    config['sdcUsername'] = secret_dict['sdc']['UserName']
-    config['sdcPassword'] = decrypt_pass(secret_dict['sdc']['Password'])
-    config['osdfPlacementUsername'] = secret_dict['osdfPlacement']['UserName']
-    config['osdfPlacementPassword'] = decrypt_pass(secret_dict['osdfPlacement']['Password'])
-    config['osdfPlacementSOUsername'] = secret_dict['osdfPlacementSO']['UserName']
-    config['osdfPlacementSOPassword'] = decrypt_pass(secret_dict['osdfPlacementSO']['Password'])
-    config['osdfPlacementVFCUsername'] = secret_dict['osdfPlacementVFC']['UserName']
-    config['osdfPlacementVFCPassword'] = decrypt_pass(secret_dict['osdfPlacementVFC']['Password'])
-    config['osdfCMSchedulerUsername'] = secret_dict['osdfCMScheduler']['UserName']
-    config['osdfCMSchedulerPassword'] = decrypt_pass(secret_dict['osdfCMScheduler']['Password'])
-    config['configDbUserName'] = secret_dict['configDb']['UserName']
-    config['configDbPassword'] = decrypt_pass(secret_dict['configDb']['Password'])
-    config['pciHMSUsername'] = secret_dict['pciHMS']['UserName']
-    config['pciHMSPassword'] = decrypt_pass(secret_dict['pciHMS']['Password'])
-    config['osdfPCIOptUsername'] = secret_dict['osdfPCIOpt']['UserName']
-    config['osdfPCIOptPassword'] = decrypt_pass(secret_dict['osdfPCIOpt']['Password'])
-    config['osdfOptEngineUsername'] = secret_dict['osdfOptEngine']['UserName']
-    config['osdfOptEnginePassword'] = decrypt_pass(secret_dict['osdfOptEngine']['Password'])
+    #secret_dict = retrieve_secrets()
+    config['osdfPlacementUsername'] = "test"
+    config['osdfPlacementPassword'] = "testpwd"
+    config['soUsername'] = ""
+    config['soPassword'] = ""
+    config['conductorUsername'] = ""
+    config['conductorPassword'] = ""
+    config['policyPlatformUsername'] = ""
+    config['policyPlatformPassword'] = ""
+    config['policyClientUsername'] = ""
+    config['policyClientPassword'] = ""
     cfg_base.http_basic_auth_credentials = creds.load_credentials(osdf_config)
     cfg_base.dmaap_creds = creds.dmaap_creds()
 
