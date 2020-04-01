0103d5d1f2fd57d538b80e8ddd5aa0eccb38a407
Sithara Nambiar <sitharav.aredath@huawei.com>
2020-04-01 17:42:04 +0530

    Route optimization test case
    Issue-ID: OPTFRA-420
    
    Signed-off-by: Sithara Nambiar <sitharav.aredath@huawei.com>
    Change-Id: I2968f60d8f222bd73553f8d4094168b8e1466906

--- test/test_simple_route_opt.py
+++ test/test_simple_route_opt.py
@@ -5,13 +5,15 @@ import unittest
 
 
 class TestSimpleRouteOptimization(unittest.TestCase):
-    @patch('apps.route.optimizers.simple_route_opt.requests.get')  # Mock 'requests' module 'get' method.
-    def test_process_nst_selection_solutions(self, mock_get):
+    @patch('apps.route.optimizers.simple_route_opt.requests.get')
+    @patch('apps.route.optimizers.simple_route_opt.pymzn.minizinc')
+    def test_process_nst_selection_solutions( self, mock_solve, mock_get):
 
         main_dir = ""
         response_data_file = main_dir + "test/simple_route_opt/AAI.json"
         mock_get.return_value.json.return_value = json_from_file(response_data_file)
         mock_get.return_value.status_code = 200
+        mock_solve.return_value = [{'x': [1, 1, 1]}]
         parameter_data_file = main_dir + "test/simple_route_opt/routeOpt.json"
         request_json = json_from_file(parameter_data_file)
         mock_response = {"requestId":"yyy-yyy-yyyy","requestStatus":"accepted","solutions":["link-id-1","link-id-2","link-id-3"],"statusMessage":" ","transactionId":"xxx-xxx-xxxx"}
@@ -23,3 +25,4 @@ class TestSimpleRouteOptimization(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
